aadf877d889532680c4416226c470a1b76ae8fff
knielbo <kln@cas.au.dk>
2020-02-25 17:31:27 +0100

    add docstring

--- tekiutil.py
+++ tekiutil.py
@@ -1,15 +1,15 @@
 import os
 
 def listFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
+    """
+    list paths to all files in directory tree in parent directory
+    Parameters:
+        dirName: str of parent directory
+    """
     listFile = os.listdir(dirName)
     allFiles = list()
-    # Iterate over all the entries
     for entry in listFile:
-        # Create full path
         fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
         if os.path.isdir(fullPath):
             allFiles = allFiles + listFiles(fullPath)
         else:
