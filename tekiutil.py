7798abc9c7d0c2bd33e25cc09e01192566a5a82b
knielbo <kln@cas.au.dk>
2020-02-28 14:54:32 +0100

    add tokenizer funcs

--- tekiutil.py
+++ tekiutil.py
@@ -1,4 +1,5 @@
 import os
+from gensim.utils import simple_preprocess
 
 def listFiles(dirName):
     """
@@ -16,3 +17,21 @@ def listFiles(dirName):
             allFiles.append(fullPath)
                 
     return allFiles
+
+#TODO: preprocessing tokenizer classe
+def sentTokenizer(sents, deacc=False):
+    """ generator tokenizer for list of strs
+    - deacc: default False with punctuation, True without
+    """
+    assert type(sents) == list, "Input has to be list"
+
+    for sent in sents:
+        yield (simple_preprocess(str(sent), deacc=deacc))
+
+def docTokenizer(docs, deacc=False):
+    """ Tokenize list of strings and return list of list of unigrams
+    - deacc: default False with punctuation, True without
+    """
+    assert type(docs) == list, "Input has to be list"
+    
+    return list(sentTokenizer(docs, deacc=deacc))
