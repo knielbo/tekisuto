192d71e6c598d56665473096c96ba7ba3922e302
knielbo <kln@cas.au.dk>
2020-03-03 21:24:38 +0100

    add id + corpus methods

--- models/latentsemantics.py
+++ models/latentsemantics.py
@@ -1,6 +1,9 @@
 """
 Class for training latent semantic models
 """
+import gensim
+import gensim.corpora as corpora
+from gensim.models import CoherenceModel
 
 class LatentSemantics:
     def __init__(self, texts, titles=False):
@@ -10,3 +13,10 @@ class LatentSemantics:
             self.titles = titles
         else:
             self.titles = ["text_{}".format(i) for i in range(len(texts))]
+
+    def generate_id2word(self):
+        return corpora.Dictionary(self.texts)
+
+    def generate_corpus(self):
+        id2word = self.generate_id2word()
+        return [id2word.doc2bow(text) for text in self.texts]
