e11b44b011a9483917bd95c014bbf26d4e64a901
knielbo <kln@cas.au.dk>
2020-03-30 15:22:12 +0200

    add stopword filtering

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -3,3 +3,4 @@ from .lemmatizer import Lemmatizer
 from .regxfilter import RegxFilter
 from .tokenizer import Tokenizer
 from .lemmatizerlatin import LemmatizerLatin
+from .swfilter import StopWordFilter
